30c4f6f45e26df83a233ba35f4ed1fab8425342b
Md Kamrul Islam <kamruldca@gmail.com>
2023-12-03 11:02:33 +0600

    push commit

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
-const ObjectId = require('mongodb').ObjectId
+const ObjectId = require("mongodb").ObjectId;
 
-// middleware 
-app.use(cors())
-app.use(express.json())
-
+// middleware
+app.use(cors());
+app.use(express.json());
 
 // console.log(process.env.DB_PASS)
 
-
-const uri =
- `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3pfigzq.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3pfigzq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,32 +28,29 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
- 
-    const userCollection = client.db('diagnoEasy').collection('users')
-    const testCollection = client.db('diagnoEasy').collection('tests')
-    const bannerCollection = client.db('diagnoEasy').collection('banners')
-    const appointmentCollection = client.db('diagnoEasy').collection('appointments')
-    
+    const userCollection = client.db("diagnoEasy").collection("users");
+    const testCollection = client.db("diagnoEasy").collection("tests");
+    const bannerCollection = client.db("diagnoEasy").collection("banners");
+    const appointmentCollection = client
+      .db("diagnoEasy")
+      .collection("appointments");
 
- 
-    app.post('/users', async(req, res) => {
-       const user = req.body
-       const result = await userCollection.insertOne(user)
-       res.send(result)
-    })
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
     app.get("/users", async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
 
-  
-
-    app.post('/tests', async(req, res) => {
-       const test = req.body
-       const result = await testCollection.insertOne(test)
-       res.send(result)
-    })
+    app.post("/tests", async (req, res) => {
+      const test = req.body;
+      const result = await testCollection.insertOne(test);
+      res.send(result);
+    });
 
     app.post("/appointments", async (req, res) => {
       const appointment = req.body;
@@ -64,16 +58,15 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/tests", async (req, res) => {
+      const result = await testCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.get('/tests', async(req, res) => {
-       const result = await testCollection.find().toArray()
-       res.send(result)
-    })
-
-    app.get('/appointments', async(req, res) => {
-       const result = await appointmentCollection.find().toArray()
-       res.send(result)
-    })
+    app.get("/appointments", async (req, res) => {
+      const result = await appointmentCollection.find().toArray();
+      res.send(result);
+    });
 
     app.get("/tests/:id", async (req, res) => {
       const id = req.params.id;
@@ -82,19 +75,24 @@ async function run() {
       res.send(result);
     });
 
-    app.post('/banners', async(req, res) => {
-       const banner = req.body
-       const result = await bannerCollection.insertOne(banner)
-       res.send(result)
-    })
+    app.post("/banners", async (req, res) => {
+      const banner = req.body;
+      const result = await bannerCollection.insertOne(banner);
+      res.send(result);
+    });
 
-     app.get("/banners", async (req, res) => {
-       const result = await bannerCollection.find().toArray();
-       res.send(result);
-     });
+    app.get("/banners", async (req, res) => {
+      const result = await bannerCollection.find().toArray();
+      res.send(result);
+    });
 
-
-
+    app.delete("/banners/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await bannerCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -108,14 +106,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('DiagnoEasy is running')
-})
-
+app.get("/", (req, res) => {
+  res.send("DiagnoEasy is running");
+});
 
 app.listen(port, () => {
-    console.log(`DiagnoEasy server is running on port ${port}`);
-})
+  console.log(`DiagnoEasy server is running on port ${port}`);
+});
